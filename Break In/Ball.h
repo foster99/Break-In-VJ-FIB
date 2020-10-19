9ae66cde0abde1b15eaf243560c73b3fb1db71d1
IDarkgenesis <byidarkgenesis@gmail.com>
2020-10-19 11:54:15 +0200

    Acabar de revisar rebots

---
+++ Break In/Ball.h
@@ -0,0 +1,27 @@
+#ifndef _BALL_INCLUDE
+#define _BALL_INCLUDE
+
+#include "Sprite.h"
+#include "TileMap.h"
+#include "Player.h"
+
+class Ball
+{
+public:
+	void init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram);
+	void update(int deltaTime);
+	void render();
+
+	void setTileMap(TileMap* tileMap);
+	void setPosition(const glm::vec2& pos);
+
+private:
+	glm::ivec2 tileMapDispl, posBall, direction;
+	Texture tex;
+	Sprite* sprite;
+	TileMap* map;
+	int sizeBall;
+};
+
+#endif // _BALL_INCLUDE
+
